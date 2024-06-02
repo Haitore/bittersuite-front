d6449385b78b0682445dea2262abf80efa40cb62
Iker Jiménez <ender12as@gmail.com>
2024-06-02 14:47:14 -0600

    Conexion a back ventas, login, vista de tiendas

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl = 'http://localhost:3000/api';
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    const route = `${this.baseUrl}/login`;
+    const body = { email, password };
+    console.log(body);
+    
+    return this.http.post<any>(route, body);
+  }
+}
--- src/app/services/stores.service.ts
+++ src/app/services/stores.service.ts
@@ -5,20 +5,14 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class SalesService {
+export class StoresService {
 
   private baseUrl = 'http://localhost:3000/api'
 
   constructor(private http: HttpClient) { }
 
-  getReport(): Observable<any[]> {
-    const route = this.baseUrl + '/sales'
+  getStores(): Observable<any[]> {
+    const route = this.baseUrl + '/hosts'
     return this.http.get<any[]>(route)
-  }
-
-  addSale(sale: any): Observable<any>{
-    const route = this.baseUrl + '/sales'
-    const body = sale
-    return this.http.post<any>(route, body)
   }
 }
