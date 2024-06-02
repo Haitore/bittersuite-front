0fb513065dee621c33eb423f547ecf3f75ebd7fc
Iker Jiménez <ender12as@gmail.com>
2024-06-02 11:59:49 -0600

    Cambiar nombre de carpeta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReportsComponent } from './components/reports/reports.component';
+import { ReportsComponent } from './views/reports/reports.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { SalesService } from './services/sales.service';
 import { AuthInterceptor } from './interceptor/auth.interceptor';
